13ade91be4ae26aec7466d08f7da77fd1343d4bf
cping01 <104638213+cping01@users.noreply.github.com>
2022-10-23 22:57:31 -0400

    Fix current election pages and little bugs/quality of life issues

--- Election-System-Web-App-Front-End/website/js/results.js
+++ Election-System-Web-App-Front-End/website/js/results.js
@@ -1,23 +1,7 @@
-/**
- * @description append an election to a list
- * 
- * @param {string} title - username of the logged in user
- * @param {string} q - query (unique) name for election
- * @param {Element} list - html element of a list
- * @param {boolean} done - is election finished?
- */
- function addElection (title, q, list, done=false) {
-    if (!done) {
-        list.innerHTML += `<a href="voting.html?q=${q}"><li>${title}</li></a>`
-    } else {
-        list.innerHTML += `<a href="result.html?q=${q}"><li>${title}</li></a>`
-    }
-}
-
 // once the document is loaded
 document.addEventListener('DOMContentLoaded', function () {
     // get list of elections
     const ALL_ELECTIONS = document.querySelector('#old ul');
     // example of adding elections with js
-    addElection('dynamic old election', q='dynamic_old', ALL_ELECTIONS, false);
+    addElection('dynamic old election', q='dynamic_old', ALL_ELECTIONS, true);
 });
--- Election-System-Web-App-Front-End/website/js/voting.js
+++ Election-System-Web-App-Front-End/website/js/voting.js
@@ -1,13 +1,97 @@
+// array to store current voting objects
+let current = [
+    {
+        q: "gif",
+        title: "GIF vs GIF",
+        description: "How is it pronounced?",
+        candidate1: {
+            name: "GIF - Soft G like 'Gift'",
+            img: "imgs/gif.jpg"
+        },
+        candidate2:  {
+            name: "GIF - Hard G like 'Giraffe'",
+            img: "imgs/jif.png"
+        }
+    },
+    {
+        q: "cereal",
+        title: "Which is first: Milk or Cereal",
+        description: "What goes in the bowl first?",
+        candidate1: {
+            name: "Milk",
+            img: "imgs/milk.jpg",
+        },
+        candidate2:  {
+            name: "Cereal",
+            img: "imgs/cereal.jpg",
+        }
+    },
+    {
+        q: "ford_vs_chevy",
+        title: "Ford vs Chevy",
+        description: "Which brand is best?",
+        candidate1: {
+            name: "Ford",
+            img: "imgs/ford.png",
+        },
+        candidate2:  {
+            name: "Chevy",
+            img: "imgs/chevy.jpg",
+        }
+    },
+    {
+        q: "apple_cherry",
+        title: "Apples vs Cherries",
+        description: "What is your favorite fruit?",
+        candidate1: {
+            name: "Apples",
+            img: "imgs/apples.jpg",
+        },
+        candidate2:  {
+            name: "Cherries",
+            img: "imgs/cherries.jpg",
+        }
+    },
+    {
+        q: "apple_samsung",
+        title: "Apple vs Samsung",
+        description: "Which is the best phone company?",
+        candidate1: {
+            name: "Apple",
+            img: "imgs/apple.png",
+        },
+        candidate2:  {
+            name: "Samsung",
+            img: "imgs/samsung.png",
+        }
+    }
+]
+
+
+/**
+ * @description gets vote info from current and displays it
+ * 
+ * @param {string} query - query word of this result
+ */
+ function displayVote (query) {
+    for (vote of current) {
+        if (vote.q === query) {
+            // display title and description
+            printTitle(vote.title);
+            printDescription(vote.description);
+            // set candidate 1 info
+            populateCandidate(1, vote.candidate1.name, vote.candidate1.img);
+            // set candidate 2 info
+            populateCandidate(2, vote.candidate2.name, vote.candidate2.img);
+        } 
+    }
+}
+
 // once the document is loaded
 document.addEventListener('DOMContentLoaded', function () {
     // how to get query word from the url
     const CURRENT_URL = new URL(window.location.href);
     const QUERY = CURRENT_URL.searchParams.get("q");
-    // print query to console
-    console.log(QUERY);
-    // display title and description
-    printTitle('dynamic election title');
-    printDescription('dynamic election description');
-    // set candidate 1 info
-    populateCandidate(1, "Dynamic Name1", 'imgs/candidate.jpg');
+    // print vote
+    displayVote(QUERY);
 });
